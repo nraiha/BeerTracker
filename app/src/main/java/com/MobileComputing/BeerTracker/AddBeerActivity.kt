37234990a08938272f4de01a1caa32605f7654b8
Nuutti Räihä <nuutti.raiha@gmail.com>
2020-03-18 22:52:17 +0200

    Go back by buttons in add beer activity

--- app/src/main/java/com/MobileComputing/BeerTracker/AddBeerActivity.kt
+++ app/src/main/java/com/MobileComputing/BeerTracker/AddBeerActivity.kt
@@ -6,12 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_add_beer.*
 
 class AddBeerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_beer)
+
+        btn_save.setOnClickListener {
+            finish();
+        }
+
+        btn_cancel.setOnClickListener {
+            finish();
+        }
     }
 
 }
--- app/src/main/java/com/MobileComputing/BeerTracker/MainView.kt
+++ app/src/main/java/com/MobileComputing/BeerTracker/MainView.kt
@@ -20,6 +20,7 @@ class MainView : Fragment() {
 
         view.btn_addBeer.setOnClickListener {
             val intent = Intent(activity, AddBeerActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent)
         }
 
