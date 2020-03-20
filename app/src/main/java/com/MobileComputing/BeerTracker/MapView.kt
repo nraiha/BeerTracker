8be028637f1c00834d1ce9de986029fab1f0171c
punkerilaura <punkeri.laura@gmail.com>
2020-03-20 13:48:36 +0200

    Add map to UI

--- app/src/main/java/com/MobileComputing/BeerTracker/MapView.kt
+++ app/src/main/java/com/MobileComputing/BeerTracker/MapView.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapView : Fragment() {
+
+class MapView : Fragment(), OnMapReadyCallback {
+
+    lateinit var map : MapView
+    lateinit var googleMap : GoogleMap
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?)
             : View? = inflater.inflate(
                     R.layout.view_map, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map!!
+    }
+
     companion object {
         fun newInstance(): MapView = MapView()
-
     }
 }
+
