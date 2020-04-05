ad537919dd432d0cbc7e06cf1381cce65e7d4f34
punkerilaura <punkeri.laura@gmail.com>
2020-04-05 15:31:55 +0300

    Init database

--- app/src/main/java/com/MobileComputing/BeerTracker/User.kt
+++ app/src/main/java/com/MobileComputing/BeerTracker/User.kt
@@ -4,7 +4,7 @@ enum class Sex { Male, Female }
 
 /*
  * Class for user.
- * Needed for calculating the drunkness
+ * Needed for calculating the drunkenness
  */
 open class User (weight: Float, sex: Sex) {
 
